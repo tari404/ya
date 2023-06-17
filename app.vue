c75a26167f68c719359ae512aa13076812d429fc
tari404 <2680383605@qq.com>
2023-06-18 00:31:37 +0800

    add seo image

--- app.vue
+++ app.vue
@@ -4,6 +4,15 @@
   </div>
 </template>
 
+<script lang="ts" setup>
+useHead({
+  meta: [
+    { hid: 'twitter:card', name: 'twitter:card', content: 'player' },
+    { hid: 'og:image', property: 'og:image', content: '/A.png' },
+  ],
+})
+</script>
+
 <style lang="sass">
 @import url('https://fonts.googleapis.com/css2?family=Grandiflora+One&family=Noto+Music&family=Noto+Serif&family=Noto+Serif+SC&display=block')
 @import '~/css/reset.sass'
